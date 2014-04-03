f150e436e74b10caad84007a07ba8c1aefdbfe5e
DJ Holt <dj@holt.dj>
2014-04-02 20:53:56 -0600

    Add API endpoints to list/search movies, get a movie, and create a movie

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from bottle import get, request, run
+from bottle import get, post, request, response, run
 import sqlite3
 
 def dict_factory(cursor, row):
@@ -11,7 +11,34 @@ db_con = sqlite3.connect("movies.db")
 db_con.row_factory = dict_factory
 db_cur = db_con.cursor()
 
-# TODO: Make an API! Put your Bottle callback functions here.
+@get("/api/movies")
+def get_movies():
+    set_headers()
+    search = request.query.search
+    if search:
+        db_cur.execute("SELECT id, name FROM movies WHERE name LIKE ?", ["%"+search+"%"])
+    else:
+        db_cur.execute("SELECT id, name FROM movies")
+    movies = db_cur.fetchall()
+    return { "movies": movies }
+
+@get("/api/movies/<movie_id>")
+def get_movie(movie_id):
+    set_headers()
+    db_cur.execute("SELECT * FROM movies WHERE id = ?", [movie_id])
+    movie = db_cur.fetchone()
+    return movie
+
+@post("/api/movies")
+def create_movie():
+    set_headers()
+    movie = request.forms
+    db_cur.execute("INSERT INTO movies (name, rating, release_date, poster_url) VALUES (?, ?, ?, ?)", [movie["name"], movie["rating"], movie["release_date"], movie["poster_url"]])
+    db_con.commit()
+    return "success"
+
+def set_headers():
+    response.set_header("Access-Control-Allow-Origin", "*")
 
 run(host="localhost", port=8080)
 
